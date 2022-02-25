70538efd5a7688d8eb8f572a4efbffb917711ae2
greg@1m <greg@1m>
2022-02-25 19:39:56 +0100

    adding heroku fake datas 1

--- server/src/managers/configSetup.manager.ts
+++ server/src/managers/configSetup.manager.ts
@@ -7,11 +7,11 @@ var fs = require('fs')
 
 // LOADING CONFIG FILE
 export interface TiroConfig {
-    user:string
-    password:string
-    dataFolder: string
-    https?: string
-    port?:string
+	user: string
+	password: string
+	dataFolder: string
+	https?: string
+	port?: string
 }
 
 /*
@@ -19,64 +19,66 @@ export interface TiroConfig {
 */
 
 // as we need to go down from sources-tiro/server/tiro-config.json to sources-tiro/tiro-config.json in dev mode
-export const appConfigJsonPath = p(`../tiro-config.json`)
+const isHeroku = process.env.PORT || false;
+
+export const appConfigJsonPath = !isHeroku ? p(`../tiro-config.json`) : p(`../misc/heroku/tiro-heroku-config.json`);
 
 let cachedJsonConfigLoadResult = null
 export const tryLoadJsonConfig = () => {
-    if (cachedJsonConfigLoadResult) return cachedJsonConfigLoadResult as TiroConfig
+	if (cachedJsonConfigLoadResult) return cachedJsonConfigLoadResult as TiroConfig
 
-    if (fileExists(appConfigJsonPath)) {
-        let res = JSON.parse(fs.readFileSync(appConfigJsonPath, 'utf8')) as TiroConfig
-        console.error('[JSON CONFIG] loaded successfully', {appConfigJsonPath, res});
-        cachedJsonConfigLoadResult = res
-        return res
-    } else {
-        console.error(`[JSON CONFIG] Json file not found at ${appConfigJsonPath}, ask frontend to display welcome screen`);
-        return null
-    }
+	if (fileExists(appConfigJsonPath)) {
+		let res = JSON.parse(fs.readFileSync(appConfigJsonPath, 'utf8')) as TiroConfig
+		console.error('[JSON CONFIG] loaded successfully', { appConfigJsonPath, res });
+		cachedJsonConfigLoadResult = res
+		return res
+	} else {
+		console.error(`[JSON CONFIG] Json file not found at ${appConfigJsonPath}, ask frontend to display welcome screen`);
+		return null
+	}
 }
 export const getDataFolder = () => {
-    const jsonConfig = tryLoadJsonConfig();
-    return (jsonConfig && jsonConfig.dataFolder) ? relativeToAbsolutePath(jsonConfig.dataFolder) : undefined
+	const jsonConfig = tryLoadJsonConfig();
+	return (jsonConfig && jsonConfig.dataFolder) ? relativeToAbsolutePath(jsonConfig.dataFolder) : undefined
 }
 export const shouldAskForSetup = () => {
-    const jsonConfig = tryLoadJsonConfig();
-    if (!jsonConfig || !jsonConfig.user || !jsonConfig.password || !jsonConfig.dataFolder) {
-        console.log('[INIT SETUP] json doesnt exists, askForSetup!');
-        return true
-    }
-    if (!fileExists(getDataFolder())) {
-        console.log('[INIT SETUP] getDataFolder() doesnt exists, askForSetup!');
-        return true
-    }
-    return false
+	const jsonConfig = tryLoadJsonConfig();
+	if (!jsonConfig || !jsonConfig.user || !jsonConfig.password || !jsonConfig.dataFolder) {
+		console.log('[INIT SETUP] json doesnt exists, askForSetup!');
+		return true
+	}
+	if (!fileExists(getDataFolder())) {
+		console.log('[INIT SETUP] getDataFolder() doesnt exists, askForSetup!');
+		return true
+	}
+	return false
 }
 
 
 
-export const processClientSetup = async (data:iApiDictionary['sendSetupInfos']):Promise<iApiDictionary['getSetupInfos']> => {
-    let answer:iApiDictionary['getSetupInfos']
+export const processClientSetup = async (data: iApiDictionary['sendSetupInfos']): Promise<iApiDictionary['getSetupInfos']> => {
+	let answer: iApiDictionary['getSetupInfos']
 
-        // check if name is > 3 chars
-        if (data.form.user.length < 3) answer = {code: 'BAD_USER_PASSWORD', message: 'user not valid'} 
-        
-        // check if password is > 3 chars
-        if (data.form.password.length < 3) answer = {code: 'BAD_USER_PASSWORD', message: 'password not valid'} 
-        
-        // check if folder provided exists
-        if (!fileExists(data.form.dataFolder)) answer = {code: 'NO_FOLDER', message: `folder path ${p(data.form.dataFolder)} doesnt exists`} 
+	// check if name is > 3 chars
+	if (data.form.user.length < 3) answer = { code: 'BAD_USER_PASSWORD', message: 'user not valid' }
 
-        // if all good
-        if (!answer ||!answer.code) {
-            // create json
-            const newConfig:TiroConfig = {
-                user: data.form.user,
-                password: await hashPassword(data.form.password),
-                dataFolder: data.form.dataFolder,
-            }
-            await saveFile(appConfigJsonPath, JSON.stringify(newConfig))
-            answer = {code: 'SUCCESS_CONFIG_CREATION'}  
-        }
+	// check if password is > 3 chars
+	if (data.form.password.length < 3) answer = { code: 'BAD_USER_PASSWORD', message: 'password not valid' }
 
-        return answer
+	// check if folder provided exists
+	if (!fileExists(data.form.dataFolder)) answer = { code: 'NO_FOLDER', message: `folder path ${p(data.form.dataFolder)} doesnt exists` }
+
+	// if all good
+	if (!answer || !answer.code) {
+		// create json
+		const newConfig: TiroConfig = {
+			user: data.form.user,
+			password: await hashPassword(data.form.password),
+			dataFolder: data.form.dataFolder,
+		}
+		await saveFile(appConfigJsonPath, JSON.stringify(newConfig))
+		answer = { code: 'SUCCESS_CONFIG_CREATION' }
+	}
+
+	return answer
 }
