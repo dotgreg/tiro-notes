b2fecf4ecca749d6dc6d3cce9dbb3179fe1706fd
greg@1m <greg@1m>
2022-03-24 18:43:16 +0100

    clean build and remove it from code source + fix pb build 9

--- client/src/components/dualView/DualViewer.component.tsx
+++ client/src/components/dualView/DualViewer.component.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect,  useState } from 'react';
-import {PreviewArea} from './PreviewArea.component'
+import React, { useEffect, useState } from 'react';
+import { PreviewArea } from './PreviewArea.component'
 import { EditorArea, onFileDeleteFn, onFileEditedFn, onLightboxClickFn, onSavingHistoryFileFn } from './EditorArea.component';
 import { iFile } from '../../../../shared/types.shared';
 import { PathModifFn } from './TitleEditor.component';
@@ -9,90 +9,88 @@ import { addCliCmd } from '../../managers/cliConsole.manager';
 
 //@TODO mobile bar
 //@TODO mobile bar func to desktop
-export type ViewType = 'editor'| 'both' | 'preview'
+export type ViewType = 'editor' | 'both' | 'preview'
 
-export const DualViewer = (p:{
-    file:iFile
-    fileContent:string
-    // viewType:ViewType
-    canEdit: boolean
-    forceRender: boolean
-    isLeavingNote: boolean
-    onFileEdited: onFileEditedFn
-    onFileTitleEdited: PathModifFn
-    onSavingHistoryFile: onSavingHistoryFileFn
-    onFileDelete: onFileDeleteFn
-    onLightboxClick: onLightboxClickFn
-    onBackButton: Function
-  }) => {
-      const {syncScrollY, updateSyncScroll, setPosY} = useSyncScroll()
+export const DualViewer = (p: {
+	file: iFile
+	fileContent: string
+	// viewType:ViewType
+	canEdit: boolean
+	forceRender: boolean
+	isLeavingNote: boolean
+	onFileEdited: onFileEditedFn
+	onFileTitleEdited: PathModifFn
+	onSavingHistoryFile: onSavingHistoryFileFn
+	onFileDelete: onFileDeleteFn
+	onLightboxClick: onLightboxClickFn
+	onBackButton: Function
+	onToggleSidebarButton: Function
+}) => {
+	const { syncScrollY, updateSyncScroll, setPosY } = useSyncScroll()
 
 
-    const [viewType, setViewType] = useLocalStorage('viewtype','both')
-    const [previewContent, setPreviewContent] = useState('')
+	const [viewType, setViewType] = useLocalStorage('viewtype', 'both')
+	const [previewContent, setPreviewContent] = useState('')
 
-    // window variables
-    addCliCmd('fileContent', {
-        description: 'live updated currentFileContent',
-        func: () => previewContent
-    })
-    
-    // useEffect(() => {
-    //     console.log('setviewtype', p.viewType);
-        
-    //     setViewType(p.viewType)
-    // }, [p.viewType])
+	// window variables
+	addCliCmd('fileContent', {
+		description: 'live updated currentFileContent',
+		func: () => previewContent
+	})
 
-    useEffect(() => {
-        setPreviewContent(p.fileContent)
-    }, [p.fileContent])
+	// useEffect(() => {
+	//     console.log('setviewtype', p.viewType);
 
-    // back to top when change file
-    useEffect(() => {
-        setPosY(1);
-        
-    }, [p.file.path])
+	//     setViewType(p.viewType)
+	// }, [p.viewType])
 
-    return <div 
-            className={`dual-view-wrapper view-${viewType}`}
-            onWheelCapture={e => {updateSyncScroll(e.deltaY)}}
-            // onTouchMoveCapture={(e:any) => {
-            //     alert('ff')
-            //     updateSyncScroll(e.deltaY)
-            // }}
-        >
-            <EditorArea
-                file={p.file}
-                posY={syncScrollY}
-                fileContent={p.fileContent}
-                canEdit={p.canEdit}
+	useEffect(() => {
+		setPreviewContent(p.fileContent)
+	}, [p.fileContent])
 
-                isLeavingNote={p.isLeavingNote}
-                
-                onScroll={newYPercent => {}}
-                onFileTitleEdited={p.onFileTitleEdited}
-                onSavingHistoryFile={p.onSavingHistoryFile}
-                onFileEdited={(path, content) => {
-                    p.onFileEdited(path, content)
-                    setPreviewContent(content)
-                }}
-                onFileDelete={p.onFileDelete}
-                onBackButton={p.onBackButton}
-                onLightboxClick={p.onLightboxClick}
-                onViewToggle={() => {
-                    if (viewType === 'both') setViewType('editor')
-                    if (viewType === 'editor') setViewType('preview')
-                    if (viewType === 'preview') setViewType('both')
-                }}
-            />
+	// back to top when change file
+	useEffect(() => {
+		setPosY(1);
 
-           <PreviewArea
-                file={p.file}
-                posY={syncScrollY}
-                fileContent={previewContent}
-           />
+	}, [p.file.path])
 
-    </div>
+	return <div
+		className={`dual-view-wrapper view-${viewType}`}
+		onWheelCapture={e => { updateSyncScroll(e.deltaY) }}
+	>
+		<EditorArea
+			file={p.file}
+			posY={syncScrollY}
+			fileContent={p.fileContent}
+			canEdit={p.canEdit}
+
+			isLeavingNote={p.isLeavingNote}
+
+			onScroll={newYPercent => { }}
+			onFileTitleEdited={p.onFileTitleEdited}
+			onSavingHistoryFile={p.onSavingHistoryFile}
+			onFileEdited={(path, content) => {
+				p.onFileEdited(path, content)
+				setPreviewContent(content)
+			}}
+			onFileDelete={p.onFileDelete}
+			onBackButton={p.onBackButton}
+			onToggleSidebarButton={p.onToggleSidebarButton}
+			onLightboxClick={p.onLightboxClick}
+			onViewToggle={() => {
+				if (viewType === 'both') setViewType('editor')
+				if (viewType === 'editor') setViewType('preview')
+				if (viewType === 'preview') setViewType('both')
+			}}
+		/>
+
+		<PreviewArea
+			file={p.file}
+			posY={syncScrollY}
+			fileContent={previewContent}
+		/>
+
+	</div>
 }
 
 
