5b4f981aa883c995bc08b27b0db945d08bdab716
greg@1m <greg@1m>
2022-04-26 15:13:45 +0200

    active/inactive tab

--- client/src/components/tabs/TabList.component.tsx
+++ client/src/components/tabs/TabList.component.tsx
@@ -41,9 +41,16 @@ const Tab = (p: {
 }) => {
 	const { tab } = { ...p }
 	return (
-		<div className="tab-wrapper">
-			<div className="tab-name"> {tab.name} </div>
-			<div className="tab-close" onClick={() => p.onUpdate('close', tab)}> x </div>
+		<div
+			className={`tab-wrapper ${tab.active ? 'active' : ''}`}
+		>
+			<div className="tab-name"
+				onClick={() => { p.onUpdate('activate', tab) }}
+			> {tab.name} </div>
+
+			<div className="tab-close"
+					 onClick={() => p.onUpdate('close', tab)}>
+					x </div>
 		</div >
 	)
 }
@@ -51,5 +58,12 @@ const Tab = (p: {
 export const StyledDiv = styled.div`
     .tab-list-wrapper {
 				padding: 20px;
+				display: flex;
+		.tab-wrapper {
+				display: flex;
+				&.active {
+						font-weight: bold
+				}
+		}
     }
 `
