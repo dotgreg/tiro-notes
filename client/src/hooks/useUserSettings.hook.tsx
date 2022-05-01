e18e49f5952cee5029d835db5c348da44a9465c9
greg@1m <greg@1m>
2022-05-01 19:02:36 +0200

    adding a few subtilities for user settings system

--- client/src/hooks/useUserSettings.hook.tsx
+++ client/src/hooks/useUserSettings.hook.tsx
@@ -2,28 +2,36 @@ import { cloneDeep } from 'lodash';
 import React, { useState } from 'react';
 import { useBackendState } from './useBackendState.hook';
 
-export interface iUserSettings {
-	sidebar?: boolean
-}
-
 type iUserSettingName =
 	'ui_sidebar' |
 	'ui_other'
 
-export type iUs = { [setting in iUserSettingName]?: any }
+export type iUserSettings = { [setting in iUserSettingName]?: any }
 
+const defaultVals: iUserSettings = {
+	ui_sidebar: true
+}
 
 export const useUserSettings = () => {
-	const [userSettings, setUserSettings, refreshUserSettingsFromBackend] = useBackendState<iUs>('user-settings', {})
+	const [userSettings, setUserSettings, refreshUserSettingsFromBackend] = useBackendState<iUserSettings>('user-settings', {})
 
 	const updateUserSetting = (name: iUserSettingName, value: any) => {
+		console.log(`[USER SETTINGS] 0011 : updateUserSettings ${name} to ${value}`);
 		const nSettings = cloneDeep(userSettings)
 		nSettings[name] = value
 		setUserSettings(nSettings)
 	}
 
+	const getUserSetting = (name: iUserSettingName) => {
+		// if settings not configured, return default
+		let res = defaultVals[name]
+		if (name in userSettings) res = userSettings[name]
+		console.log(`[USER SETTINGS] 0011 : get ${name} > ${res}`);
+		return res
+	}
+
 	return {
-		userSettings,
+		getUserSetting,
 		updateUserSetting,
 		refreshUserSettingsFromBackend
 	}
