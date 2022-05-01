3e774830c90e92e706a7ef7ab41a0c37ad8ec8db
greg@1m <greg@1m>
2022-05-01 20:00:41 +0200

    UX bugfix tabs et windows

--- client/src/components/dualView/DualViewer.component.tsx
+++ client/src/components/dualView/DualViewer.component.tsx
@@ -71,10 +71,7 @@ export const DualViewer = (p: {
 	}, [p.file.path])
 
 
-	const dualWrapperEl = useRef<HTMLDivElement>(null)
-
 	return <div
-		ref={dualWrapperEl}
 		className={`dual-view-wrapper view-${p.viewType} device-${deviceType()}`}
 		onWheelCapture={e => { updateSyncScroll(e.deltaY) }}
 	>
