ca2181a6dfc26283e4aa0a942733e01a8cc942b8
greg@1m <greg@1m>
2022-08-20 09:54:10 +0200

    deleting tab now requires a prompt

--- client/src/components/tabs/TabList.component.tsx
+++ client/src/components/tabs/TabList.component.tsx
@@ -3,143 +3,150 @@ import { iTab } from '../../../../shared/types.shared';
 import { ClientApiContext } from '../../hooks/api/api.hook';
 import { onTabUpdateFn } from '../../hooks/app/tabs.hook';
 import { deviceType } from '../../managers/device.manager';
+import { strings } from '../../managers/strings.manager';
 import { cssVars } from '../../managers/style/vars.style.manager';
 import { Icon } from '../Icon.component';
 
 export const TabList = (p: {
-				tabs: iTab[]
-							onUpdate: onTabUpdateFn
-		}) => {
+	tabs: iTab[]
+	onUpdate: onTabUpdateFn
+}) => {
 
-		const api = useContext(ClientApiContext);
+	const api = useContext(ClientApiContext);
 
-		const [dragId, setDragId] = useState(-1)
+	const [dragId, setDragId] = useState(-1)
 
-		return (
-				<div className="tab-list-scroll-wrapper">
-				<div className="tab-list-invisible-scroll">
+	return (
+		<div className="tab-list-scroll-wrapper">
+			<div className="tab-list-invisible-scroll">
 				<div className="tab-list-wrapper">
-				{/* ALL TABS LIST*/}
-				{p.tabs.map((tab, i) =>
-										<Tab
-										key={i}
-										tab={tab}
+					{/* ALL TABS LIST*/}
+					{p.tabs.map((tab, i) =>
+						<Tab
+							key={i}
+							tab={tab}
 
 
-										onDrop={pos => { setDragId(pos) }}
-										onDragEnter={pos => { setDragId(pos) }}
-										onDragEnd={pos => {
-																			api && api.tabs.reorder(pos, dragId)
-																			setDragId(-1)
-																	}}
-										showDragIndic={dragId === i}
-										pos={i}
+							onDrop={pos => { setDragId(pos) }}
+							onDragEnter={pos => { setDragId(pos) }}
+							onDragEnd={pos => {
+								api && api.tabs.reorder(pos, dragId)
+								setDragId(-1)
+							}}
+							showDragIndic={dragId === i}
+							pos={i}
 
-										onUpdate={p.onUpdate}
-										/>
-)}
+							onUpdate={p.onUpdate}
+						/>
+					)}
 
-				{/* ADD NEW TAB BUTTON*/}
-				{api?.ui.browser.files.active.get &&
+					{/* ADD NEW TAB BUTTON*/}
+					{api?.ui.browser.files.active.get &&
 
-				 <div
-				 className="tab-wrapper tab-more"
-				 onClick={e => {
-												 p.onUpdate('add')
-										 }}
-				 >
-				 +
-				 </div>
-				}
+						<div
+							className="tab-wrapper tab-more"
+							onClick={e => {
+								p.onUpdate('add')
+							}}
+						>
+							+
+						</div>
+					}
 
 				</div>
-				</div>
-				</div>
-		)
+			</div>
+		</div>
+	)
 
 }
 
 
 const Tab = (p: {
-				tab: iTab
-						 onUpdate: onTabUpdateFn
+	tab: iTab
+	onUpdate: onTabUpdateFn
 
-											 onDragEnter: Function
-																		onDragEnd: Function
-																							 onDrop: Function
+	onDragEnter: Function
+	onDragEnd: Function
+	onDrop: Function
 
-																											 pos: number
-																														showDragIndic: boolean
-		}) => {
-		const { tab, pos } = { ...p }
+	pos: number
+	showDragIndic: boolean
+}) => {
+	const { tab, pos } = { ...p }
 
-		let iconName = `Six`
-		const nbLay = tab.grid.layout.length
-		if (nbLay === 2) iconName = 'Two'
-		if (nbLay === 3) iconName = 'Three'
-		if (nbLay === 4) iconName = 'Four'
-		if (nbLay === 5) iconName = 'Five'
-		iconName = `faDice${iconName}`
+	let iconName = `Six`
+	const nbLay = tab.grid.layout.length
+	if (nbLay === 2) iconName = 'Two'
+	if (nbLay === 3) iconName = 'Three'
+	if (nbLay === 4) iconName = 'Four'
+	if (nbLay === 5) iconName = 'Five'
+	iconName = `faDice${iconName}`
 
+	const api = useContext(ClientApiContext);
 
-
-		return (//jsx
-						<div className="tab-and-drag-wrapper">
-		{
+	return (//jsx
+		<div className="tab-and-drag-wrapper">
+			{
 				p.showDragIndic && <div className="drag-indic">
-				{/* <Icon name="faPlusCircle" color={`#b2b2b2`} />*/}
-				▼
+					{/* <Icon name="faPlusCircle" color={`#b2b2b2`} />*/}
+					▼
 				</div>
-		}
-						<div
-						className={`tab-wrapper ${tab.active ? 'active' : ''}`}
-						draggable={true}
-						onDragEnter={() => { p.onDragEnter(pos) }}
-						onDrop={() => { p.onDrop(pos) }}
-						onDragEnd={() => { p.onDragEnd(pos) }}
-						>
+			}
+			<div
+				className={`tab-wrapper ${tab.active ? 'active' : ''}`}
+				draggable={true}
+				onDragEnter={() => { p.onDragEnter(pos) }}
+				onDrop={() => { p.onDrop(pos) }}
+				onDragEnd={() => { p.onDragEnd(pos) }}
+			>
 
-						<div className="active-ribbon"></div>
-						<div className="tab-name"
+				<div className="active-ribbon"></div>
+				<div className="tab-name"
+					onClick={() => { p.onUpdate('activate', tab) }}
+				>
+
+					{deviceType() !== 'mobile' &&
+						< input
+							type="text"
+							className="tab-input-text"
+							value={tab.name}
+							style={{ width: tab.name.length * 10 + "px" }}
+							onChange={e => {
+								p.onUpdate('rename', tab, e.target.value)
+							}}
+						/>
+					}
+					{deviceType() === 'mobile' &&
+						<>{tab.name}</>
+					}
+
+
+				</div>
+
+
+				{
+					tab.grid.layout.length > 1 &&
+					<div
+						className="tab-nb-windows"
 						onClick={() => { p.onUpdate('activate', tab) }}
-						>
+					>
+						<Icon name={iconName} color={`#b2b2b2`} />
+					</div>
+				}
 
-						{deviceType() !== 'mobile' &&
-						 < input
-						 type="text"
-						 className="tab-input-text"
-						 value={tab.name}
-						 style={{ width: tab.name.length * 10 + "px" }}
-						 onChange={e => {
-															p.onUpdate('rename', tab, e.target.value)
-													}}
-						 />
-						}
-						{deviceType() === 'mobile' &&
-						 <>{tab.name}</>
-						}
-
-
-						</div>
-
-
-						{
-								tab.grid.layout.length > 1 &&
-								<div
-								className="tab-nb-windows"
-								onClick={() => { p.onUpdate('activate', tab) }}
-								>
-								<Icon name={iconName} color={`#b2b2b2`} />
-																												</div>
-						}
-
-						< div className="tab-close"
-						onClick={() => p.onUpdate('close', tab)}>
-						<Icon name="faPlus" color={`#b2b2b2`} />
-																									</div>
-																									</div >
-																									</div >
-)//jsx
+				< div className="tab-close"
+					onClick={() => {
+						api && api.popup.confirm(
+							`${strings.deleteTabPrompt} "${tab.name}"?`, () => {
+								p.onUpdate('close', tab)
+							});
+					}
+					}>
+					<Icon name="faPlus" color={`#b2b2b2`} />
+				</div>
+			</div >
+		</div >
+	)//jsx
 }
 
 export const tabsCss = () => `
