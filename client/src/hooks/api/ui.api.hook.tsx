39e8e8b1be46e61df0e28e820e2c8b415e72e171
greg@1m <greg@1m>
2022-12-31 12:19:07 +0100

    improving epub reader adding next-prev overlay + fullscreen + fontsize + tts 1/2

---
+++ client/src/hooks/api/ui.api.hook.tsx
@@ -0,0 +1,14 @@
+import { iWindowsApi } from "../app/tabs.hook";
+import { iLightboxApi } from "../app/useLightbox.hook";
+import { iBrowserApi } from "./browser.api.hook";
+import { iNoteApi } from "./note.api.hook";
+import { iSearchUiApi } from "./search.hook.api";
+
+export interface iUiApi {
+	browser: iBrowserApi
+	windows: iWindowsApi
+	lightbox: iLightboxApi
+	search: iSearchUiApi
+	note: iNoteApi["ui"]
+	// textToSpeechPopup: iTextToSpeechApi
+}
