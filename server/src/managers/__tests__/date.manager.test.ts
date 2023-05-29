cce5ea94c5a8abfd21bf2eaaa0fadbdab732599a
greg@raw3 <greg@raw3>
2023-05-29 16:16:21 +0000

    adding date system of reading/writing custom date format

---
+++ server/src/managers/__tests__/date.manager.test.ts
@@ -0,0 +1,12 @@
+import { getDateObj } from "../../../../shared/helpers/date.helper";
+
+test('getDateObj: generating and reading custom formats',  () => {
+    const cf1 = getDateObj("w4-03-2023")
+    expect(cf1.getCustomFormat("week")).toStrictEqual("w4-03-2023");
+    
+    const cf2 = getDateObj("d27-03-2022")
+    expect(cf2.getCustomFormat("day")).toStrictEqual("d27-03-2022");
+
+    const cf3 = getDateObj("f03-31-2021_21h13m")
+    expect(cf3.getCustomFormat("full")).toStrictEqual("f03-31-2021_21h13m");
+})
