9a9dda92532f8c7ff30dfc9be563093b7983e807
greg@1m <greg@1m>
2023-06-02 22:23:28 +0200

    testing env prep in a script

--- server/src/managers/testingEnv.manager.ts
+++ server/src/managers/testingEnv.manager.ts
@@ -1,17 +1,16 @@
 import { TiroConfig } from "./configSetup.manager"
-import { createFolder, deleteFolder } from "./fs.manager";
+import { getTestingDataPath} from "../../scripts/prepareTestingEnv.script";
 
 export const setupTestingEnv = () => {
 
 
 }
 
-export const getTestingEnvJsonConfig =  (): TiroConfig => {
+
+export const getTestingEnvJsonConfig = (): TiroConfig => {
 
 	// dataFolder is cleaned then recreated under the root user folder 
 	// done asynced
-	const homedir = require('os').homedir();
-	const pathDataFolder = require('path').join(homedir, `/.tiro_testing_env_data_folder`)
 
 	// await deleteFolder(pathDataFolder)
 	// await createFolder(pathDataFolder)
@@ -20,6 +19,6 @@ export const getTestingEnvJsonConfig =  (): TiroConfig => {
 	return {
 		"user": "test",
 		"password": "34da3782d9fcefaca610fa2c965766a6:51c84351eea5384c60be41b8b4869ae46f9f6c9911346d453dfc17cd6f2bdab09dd65f0d5bd1876a0f5808abc49ecf69a3386893f883534e75a6db227648fa9f",
-		"dataFolder": pathDataFolder
+		"dataFolder": getTestingDataPath()
 	}
 }
