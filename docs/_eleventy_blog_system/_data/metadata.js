53801145c33669da4bd8825dcd41480da15233c0
greg@1m <greg@1m>
2023-06-06 22:38:09 +0200

    adding eleventy blogging system

---
+++ docs/_eleventy_blog_system/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+		title: "tiro notes blog",
+		url: "https://example.com/",
+		language: "en",
+		description: "I am writing about my experiences as a naval navel-gazer.",
+		author: {
+				name: "tiro notes",
+				email: "youremailaddress@example.com",
+				url: "https://example.com/about-me/"
+		}
+}
