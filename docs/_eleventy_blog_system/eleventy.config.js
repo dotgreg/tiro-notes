a3f5235bcf27e4ef588b7edf3ce773f62f8890eb
greg@1m <greg@1m>
2023-06-09 20:09:51 +0200

    testing url blog again

--- docs/_eleventy_blog_system/eleventy.config.js
+++ docs/_eleventy_blog_system/eleventy.config.js
@@ -134,6 +134,6 @@ module.exports = function(eleventyConfig) {
 				// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
 				// it will transform any absolute URLs in your HTML to include this
 				// folder name and does **not** affect where things go in the output folder.
-				pathPrefix: "/",
+				pathPrefix: "/blog/",
 		};
 };
