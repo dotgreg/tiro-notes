31464195bca9f73e3ddfcbca9b72b919b55ea468
greg@raw3 <greg@raw3>
2023-06-14 16:52:42 +0000

    improving pandoc export stability + fixing images broken

--- client/src/managers/export.manager.ts
+++ client/src/managers/export.manager.ts
@@ -8,116 +8,116 @@ import { getApi } from "../hooks/api/api.hook"
 import { getStaticRessourceLink } from "./ressource.manager"
 import { ssrFn } from "./ssr.manager"
 
+const h = `[EXPORT]`
 
-	//
-	export const triggerExportPopup = (file: iFile) => {
-		// OLD
-		
+const downloadFile = (newFileName:string, pathToFile:string) => {
+    // const popup = document.getElementById('export-popup-wrapper')
+    // if (!popup) return console.warn('no popup for downloadFile')
+    // popup.innerHTML = popup.innerHTML + `<iframe id="my_iframe" style="display:none;" src="${pathToFile}"></iframe>`
+    var anchor = document.createElement('a');
+    anchor.setAttribute("download", newFileName);
+    anchor.setAttribute("href", pathToFile);
+    anchor.setAttribute("target", "_blank");
+    getApi(api => {api.ui.notification.emit({ content: `[EXPORT] done`, id:"export", options:{hideAfter: 3} })})
+    anchor.click();
+}
 
-        const downloadFile = (pathToFile:string) => {
-            // const popup = document.getElementById('export-popup-wrapper')
-            // if (!popup) return console.warn('no popup for downloadFile')
-            // popup.innerHTML = popup.innerHTML + `<iframe id="my_iframe" style="display:none;" src="${pathToFile}"></iframe>`
-            var anchor = document.createElement('a');
-            anchor.setAttribute("download", "true");
-            anchor.setAttribute("href", pathToFile);
-            anchor.setAttribute("target", "_blank");
-            anchor.click();
-        }
-        const h = `[EXPORT]`
+const exportTo = (el) => {
+    if (!el) return
+    let format = el.dataset.format
+    let path = el.dataset.path
+    console.log(h,0, {format, path})
+    // getting the file
+    if (!format || !path) return
+    let ssrfile = pathToIfile(path)
+    // get the absolute path of data folder
+    getApi(api => {
+        getApi(api => {api.ui.notification.emit({ content: `[EXPORT] converting...`, id:"export", options:{hideAfter: -1} })})
+        console.log(h,1)
+        api.config.get(cnf => {
+            
+            const destCacheFolder = `/${sharedConfig.path.configFolder}/${sharedConfig.path.cacheFolder}/pandoc`
+            // create folders if does not exists
+            api.folders.create(destCacheFolder, status => {
+                
+                
+                //
+                // ABSOLUTE (not working)
+                //
+                // const newFileName = `${ssrfile.name}.${format}`
+                // const inputFile = cleanPath(`${cnf.dataFolder}/${ssrfile.path}`)
+                // const inputFolder = cleanPath(`${cnf.dataFolder}/${ssrfile.folder}`)
+                // const resourceFolder = cleanPath(`${inputFolder}/${sharedConfig.path.uploadFolder}`)
+                // 1
+                // const destPath = cleanPath(`${destCacheFolder}/${newFileName}`)
+                // const destDlPath = cleanPath(`/${sharedConfig.path.configFolder}/${sharedConfig.path.cacheFolder}/pandoc/${newFileName}`)
+                // 2
+                // const destPath = cleanPath(`${resourceFolder}/${newFileName}`)
+                // const destDlPath = cleanPath(`/${ssrfile.folder}/${sharedConfig.path.uploadFolder}/${newFileName}`)
+                //
+                // RELATIVE + CD 
+                //
+                const newFileName = `${ssrfile.name}.${format}`
+                const rootPath = `${cnf.dataFolder}/`
+                const pathToCd = cleanPath(`${rootPath}/${ssrfile.folder}`)
+                const inputFilePath = cleanPath(`${ssrfile.name}`)
+                const destPathAbs = cleanPath(`${rootPath}/${destCacheFolder}/${newFileName}`)
+                const destDlPath = cleanPath(`/${sharedConfig.path.configFolder}/${sharedConfig.path.cacheFolder}/pandoc/${newFileName}`)
 
-        // const exportToOld = () => {
-        //     const currView:iViewType = p.viewType || "editor"
-        //     // if we are editor, make preview appearing for a moment 
-        //     if (currView === "editor") {
-        //         askForViewToggle("both")
-        //         setTimeout(() => {
-        //             openExportFilePopup(p.windowId, p.file)
-        //             setTimeout(() => {
-        //                 askForViewToggle("editor")
-        //             })
-        //         }, 100)
-        //     } else {
-        //         openExportFilePopup(p.windowId, p.file)
-        //     }
-        // }
-        
-        const exportTo = (el) => {
-            if (!el) return
-            let format = el.dataset.format
-            let path = el.dataset.path
-            // getting the file
-            if (!format || !path) return
-            let ssrfile = pathToIfile(path)
-            // get the absolute path of data folder
-            getApi(api => {
-                console.log(h,1)
-                api.config.get(cnf => {
-                    const destCacheFolder = `${cnf.dataFolder}/${sharedConfig.path.configFolder}/${sharedConfig.path.cacheFolder}/pandoc`
-                    console.log(h,2)
-                    // create folders if does not exists
-                    api.folders.create(destCacheFolder, status => {
-                        const newFileName = `${file.name}.${format}`
-                        const destPath = cleanPath(`${destCacheFolder}/${newFileName}`)
-                        const inputFolder = cleanPath(`${cnf.dataFolder}/${file.path}`)
-                        
-                        let cmd = ``
-                        // if (format === "pdf") cmd = `pandoc -o '${destPath}' --pdf-engine=xelatex -f markdown -t ${format} '${inputFolder}'`
-                        if (format === "pdf") cmd = `pandoc -o '${destPath}' -f markdown -t ${format} '${inputFolder}'`
-                        if (format === "docx") cmd = `pandoc -o '${destPath}' -f markdown -t ${format} '${inputFolder}'`
-                        if (format === "pptx") cmd = `pandoc -o '${destPath}' -f markdown -t ${format} '${inputFolder}'`
-                        
+                const pandocCmd = `cd ${pathToCd} && pandoc -o "${destPathAbs}" -f markdown -t ${format} "${inputFilePath}" --toc `
+                
 
-                        console.log(h,3,{format, cmd, destPath, inputFolder, ssrfile, destCacheFolder})
+                console.log(h,3,{format, pandocCmd, destPathAbs, inputFilePath,  ssrfile, destCacheFolder,  destDlPath})
 
-                        // execute pandoc cmd into cache/export/file.fdsljfdsalkfjdsalj.ppt
-                        api.command.exec(cmd, (res) => {
-                            let resObj  = {failed:false, stderr:null, shortMessage: null}
-                            try {
-                                console.log(h,4)
-                                resObj = JSON.parse(res) || {failed:false}
-                            } catch (e) {
-                                console.log(h,5)
-                            }
-                            
-                            console.log(resObj)
-                            if (resObj.failed) {
-                                console.log(h,6)
-                                api.ui.notification.emit({
-                                    content: `[EXPORT] Error <br/> "${resObj.stderr} <br/><br/> ${resObj.shortMessage}"`
-                                })
-                            } else {
-                                console.log(h,7)
-                                const destDlFolder = `/${sharedConfig.path.configFolder}/${sharedConfig.path.cacheFolder}/pandoc`
-                                const destDlPath = `/${destDlFolder}/${newFileName}`
-                                // trigger download
-                                let ressLink = getStaticRessourceLink(destDlPath)
-                                downloadFile(ressLink)
+                // execute pandoc pandocCmd into cache/export/file.fdsljfdsalkfjdsalj.ppt
+                api.command.exec(pandocCmd, (res) => {
+                    let resObj  = {failed:false, stderr:null, shortMessage: null}
+                    try {
+                        console.log(h,4)
+                        resObj = JSON.parse(res) || {failed:false}
+                    } catch (e) {
+                        console.log(h,5)
+                    }
+                    
+                    console.log(resObj)
+                    if (resObj.failed) {
+                        console.log(h,6)
+                        api.ui.notification.emit({ content: `[EXPORT] Error <br/> "${resObj.stderr} <br/><br/> ${resObj.shortMessage}"`, id:"export" })
+                    } else {
+                        console.log(h,7)
+                       
+                        // trigger download
+                        let ressLink = getStaticRessourceLink(destDlPath)
+                        downloadFile(newFileName, ressLink)
 
-                                // delete that file after 2mi
-                            }
-                        })
-                    })
+                        // delete that file after 2mi
+                    }
                 })
             })
-        }
+        })
+    })
+}
 
-        getApi(api => {
-			api.popup.show(`
-			<div id="export-popup-wrapper">
-                <div class="buttons-list"> 
-                    <button onclick="${ssrFn("open-win-ress", exportTo)}" data-format="docx" data-path="${file.path}">docx</button>
-                    <button onclick="${ssrFn("open-win-ress", exportTo)}" data-format="pdf" data-path="${file.path}">pdf</button>
-                    <button onclick="${ssrFn("open-win-ress", exportTo)}" data-format="pptx" data-path="${file.path}">pptx</button>
-                </div>
+export const triggerExportPopup = (file: iFile) => {
+    getApi(api => {
+        api.popup.show(`
+        <div id="export-popup-wrapper">
+            <div class="buttons-list"> 
+                <button onclick="${ssrFn("export-note-to", exportTo)}" data-format="docx" data-path="${file.path}">docx</button>
+                <button onclick="${ssrFn("export-note-to", exportTo)}" data-format="pdf" data-path="${file.path}">pdf</button>
+                <button onclick="${ssrFn("export-note-to", exportTo)}" data-format="pptx" data-path="${file.path}">pptx</button>
+            </div>
 
-                <div class="advice">  
-                    <br/>
-                    Please make sure you have Pandoc v2.9 installed on your system to have docx/ppt exports working. 
-                    For PDF, make sure xelatex is installed
-                    <a href="https://pandoc.org/installing.html" target="_blank">Guide here </a>
-                </div>
-			</div>
-			`, "Export Note", (ttt) => {console.log(ttt)})
-		})
-	}
+            <div class="advice">  
+                <br/>
+                Please make sure you have Pandoc v2.9 installed on your system to have docx/ppt exports working. 
+                <a href="https://pandoc.org/installing.html" target="_blank">Guide here </a>
+                
+                <br/><br/>
+                For PDF, make sure pdflatex is installed 
+                <br/> (if you are on termux: <code>pkg install texlive-installer texlive-tlmgr; termux-install-tl</code>)
+            </div>
+        </div>
+        `, "Export Note to :", (ttt) => {console.log(ttt)})
+    })
+}
