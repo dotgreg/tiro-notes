8af2af8dc12c5752493522ad4df1796fc9615987
greg@raw3 <greg@raw3>
2023-07-16 16:36:23 +0000

    working generator script 1/2 marketplace

--- docs/_marketplace_generator/marketplace.ts
+++ docs/_marketplace_generator/marketplace.ts
@@ -1,8 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+
 import { each } from "lodash"
+const h = "[MARKETPLACE GEN]"
 
-// for each ./plugin/pluginName/
-    // look for pluginName.plugin.js
-    // add it to marketplace.json
+const paths = {
+    pluginsFolder: "../../plugins",
+    finalJsonPath: "../marketplace.json"
+}
+    
+const generateMarketplaceJson = (): void => {
+    let folderPathToScan: string = paths.pluginsFolder
+    const marketplaceArray: any[] = [];
 
-// save marketplace.json in root /docs folder
-    // even if it gonna be accessed using https://github.com/dotgreg/tiro/master/docs/marketplace.json (can be modified)
+    const scanFolder = (folderPath: string): void => {
+        const files = fs.readdirSync(folderPath);
+
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const stat = fs.statSync(filePath);
+
+            if (stat.isDirectory()) {
+                scanFolder(filePath); // Recursively scan subfolders
+            } else if (file.endsWith('.plugin.js')) {
+                console.log(h, 'plugin file found! =>', file);
+                const pluginContent = fs.readFileSync(filePath, 'utf-8');
+                const pluginArray = new Function(pluginContent)() 
+                marketplaceArray.push(...pluginArray);
+            }
+        }
+    };
+
+    scanFolder(folderPathToScan);
+
+    const jsonContent = JSON.stringify(marketplaceArray, null, 2);
+    fs.writeFileSync(paths.finalJsonPath, jsonContent);
+
+    console.log(h, 'Marketplace JSON file generated successfully.');
+};
+
+generateMarketplaceJson()
