83fd00bf7415bac48ed06cb8a5761b5a1c9a6408
greg@raw3 <greg@raw3>
2023-07-25 17:56:44 +0000

    fix macos crop feed

--- client/src/managers/device.manager.ts
+++ client/src/managers/device.manager.ts
@@ -19,7 +19,21 @@ export const isA = (device: iDeviceType): boolean => {
 }
 
 
-
+function getOperatingSystem() {
+	const platform = navigator.platform.toLowerCase();
+  
+	if (platform.includes('mac')) {
+	  return 'mac';
+	} else if (platform.includes('win')) {
+	  return 'windows';
+	} else if (platform.includes('linux')) {
+	  return 'linux';
+	} else if (platform.includes('android')) {
+	  return 'android';
+	} else {
+	  return 'other';
+	}
+  }
 
 export function isIpad() {
 	if (/iPad/.test(navigator.platform)) {
