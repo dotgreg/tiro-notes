80fc55c4b401b86b7da1d3ea3c9e261182de779f
greg@ksv3-prod-server <greg@ksv3-prod-server>
2024-04-15 07:41:00 +0000

    switch epub back to paginated + throttling added for timeline logging

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.41.42";
+const version = "0.41.43";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
