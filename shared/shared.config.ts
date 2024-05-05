4a86046fe4385525b795392bc2ec28fdfb9756f1
greg@ksv3-prod-server <greg@ksv3-prod-server>
2024-05-05 12:59:38 +0000

    fixing search dates wrong

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.41.72";
+const version = "0.41.73";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
