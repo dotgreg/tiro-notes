7c474597a2fcef7321f40167d881e0cc18432834
greg@ksv3-prod-server <greg@ksv3-prod-server>
2024-05-06 15:19:58 +0000

    adding created col in smartlist

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.41.79";
+const version = "0.41.80";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
