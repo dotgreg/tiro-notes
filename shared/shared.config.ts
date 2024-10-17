45f3653fb69120f9f7d7d660b808a68c7cf543f5
greg@ksv3-prod-server <greg@ksv3-prod-server>
2024-10-17 20:08:08 +0000

    up

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.42.28";
+const version = "0.42.29";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
