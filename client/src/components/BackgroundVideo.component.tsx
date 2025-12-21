66ce5f7f50b7d19dbbeaf2157c156e69566f67fd
greg@ksv3-prod-server <greg@ksv3-prod-server>
2025-12-21 20:10:50 +0000

    making background url video more flexible

--- client/src/components/BackgroundVideo.component.tsx
+++ client/src/components/BackgroundVideo.component.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { getLoginToken } from '../hooks/app/loginToken.hook';
 export const BackgroundVideo = (p:{
     url: string
     width: number
@@ -7,12 +8,23 @@ export const BackgroundVideo = (p:{
     let url = p.url;
     url = url.trim()
     // if ? exists, just add &autoplay=1, otherwise add ?autoplay=1
-    if (url.includes('?')) {
-        url += '&autoplay=1&mute=1';
-    } else {
-        url += '?autoplay=1&mute=1';
+    
+    // if url contains [[token]], replaces it
+    if (url.includes("[[token]]")) {
+        url = url.replace("[[token]]", getLoginToken());
     }
 
+    // if url includes < or >, it is raw HTML, insert it as it is
+    let isRawHTML = false;
+    if (url.includes('<') || url.includes('>')) {
+        isRawHTML = true;
+    }else {
+        if (url.includes('?')) {
+            url += '&autoplay=1&mute=1';
+        } else {
+            url += '?autoplay=1&mute=1';
+        }
+    }
 
     const [show, setShow] = useState(false);
 
@@ -23,19 +35,27 @@ export const BackgroundVideo = (p:{
     }, []);
 
     return (
-        <div className="background-video" style={{width: `${p.width}%`, height:`${p.height}%`} }>
-            {show && <iframe  
+
+        <div id="background-video" className={`background-video ${isRawHTML ? 'html' : 'iframe'}`} style={{width: `${p.width}%`, height:`${p.height}%`} }>
+            {show && !isRawHTML && <iframe  
             src={`${url}`} 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             >
 
             </iframe>}
+            {show && isRawHTML && <div className='html-wrapper-raw' dangerouslySetInnerHTML={{__html: url}}></div>}
         </div>
     );
 }
 export const BackgroundVideoCSS = () => `
+.html-wrapper-raw {
+    width: 100%;
+    height: 100%;
+}
 .background-video {
     position: fixed;
+}
+.background-video.iframe {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
