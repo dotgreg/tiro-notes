d20670d3b371b558473c38b1ce3eecf669ba6c05
greg@ksv3-prod-server <greg@ksv3-prod-server>
2026-01-21 10:46:02 +0000

    adding ismobile to app-title

--- client/src/managers/pageTitle.manager.ts
+++ client/src/managers/pageTitle.manager.ts
@@ -1,21 +1,22 @@
 import { getApi } from "../hooks/api/api.hook";
+import { isMobile } from "./device.manager";
 import { evalCode } from "./evalCode.manager";
 
-export const updatePageTitle = (title?: string, disableTitleNoteConfig?:boolean) => {
-    let defaultTitle = "Tiro"
-    if (!title) title = defaultTitle;
-    if (disableTitleNoteConfig) {
-        document.title = title;
-        return;
-    }
 
+export const updatePageTitle = (title?: string, forceTitle?:boolean) => {
+    let defaultTitle = "Tiro"
+    console.log("Updating page title to:", title, forceTitle);
+    if (!title) title = defaultTitle;
+    if (forceTitle) document.title = title;
+    if (forceTitle) return
+    console.log("Checking for custom title...");
     getApi(api => {
         // check if note /.tiro/pagetitle.md exists
         api.file.getContent("/.tiro/app-title.md", noteContent => {
             if (noteContent === "NO_FILE") {
                 document.title = title || defaultTitle;
             } else {
-                evalCode(noteContent, {"api":api}, (result) => {
+                evalCode(noteContent, {"api":api, isMobile}, (result) => {
                     document.title = result || defaultTitle;
                 });
             }
