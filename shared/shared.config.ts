cb6b499299f30a85655d07a3f4288198eabc9193
greg@ksv3-prod-server <greg@ksv3-prod-server>
2026-02-16 10:18:03 +0000

    fix bug table

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.51.46";
+const version = "0.51.47";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
