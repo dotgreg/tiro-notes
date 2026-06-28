e5659a870e550a55f0fe75575d5f01b63ef0ef12
greg <greg>
2026-06-28 17:06:49 +0200

    fix: URL regex require dot+TLD min 2chars

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.51.89";
+const version = "0.51.91";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
