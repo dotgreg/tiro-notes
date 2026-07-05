0c883ebde1c4844eeddd22a964a1a15a0d14a8e6
greg <greg>
2026-07-05 18:35:15 +0200

    add epub ctag version 0.2 to help

--- shared/shared.config.ts
+++ shared/shared.config.ts
@@ -1,6 +1,6 @@
 import { sharedStrings } from './shared.strings'
 // THIRD LINE AUTOMATICALLY GENERATED, SHOULD ALWAYS BE THE THIRD LINE!
-const version = "0.51.95";
+const version = "0.51.96";
 // END OF AUTOMATICALLY GENERATED BLOCK
 
 export const sharedConfig = {
